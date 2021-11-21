514dabf7b97e6d9902d1327831f661da75b55160
siweh <sanemadesi@gmail.com>
2021-11-21 18:30:55 +0200

    Fix the travis file

--- insert-data.sql
+++ insert-data.sql
@@ -1,5 +1,6 @@
-insert into fruit (name, price) values ('Apples', 4.77);
-insert into fruit (name, price) values ('Bananas', 5.65);
-insert into fruit (name, price) values ('Pears', 8.37);
-insert into fruit (name, price) values ('Oranges', 6.75);
-insert into fruit (name, price) values ('Grapes', 23.95);
+-- insert into fruit (name, price) values ('Apples', 4.77);
+-- insert into fruit (name, price) values ('Bananas', 5.65);
+-- insert into fruit (name, price) values ('Pears', 8.37);
+-- insert into fruit (name, price) values ('Oranges', 6.75);
+-- insert into fruit (name, price) values ('Grapes', 23.95);
+insert into fruit (name, price) values ('Mango', 7.95);
